14517e79c23db77378ec16cb33074b2d7c51e8c9
vladrabets <59253333+vladrabets@users.noreply.github.com>
2020-10-25 21:25:29 +0300

    Add files via upload

--- Laba2Rabets/Laba2.js
+++ Laba2Rabets/Laba2.js
@@ -36,15 +36,15 @@ let Student = [
   
 ] 
 
-let iter = Object.keys(Student[0]);
-
 function getField(i, j) { 
-  console.log(i, j);
   return Object.values(Student[i])[j];
 }
 
+let fieldName = Object.keys(Student[0]);
+
 return{
-    appendTable : function(){
+    printTable : function(){
+
         let table = body.appendChild(document.createElement('table'));
         
         let tr = document.createElement('tr');
@@ -56,20 +56,26 @@ return{
           table.appendChild(tr);
 
           if (i == -1){
-            for (let l = 0; l < iter.length; l++) {
+
+            for (let j = 0; j < fieldName.length; j++) {
+
               let th = document.createElement('th');
               tr.appendChild(th);
-              th.innerText = iter[l];
+              th.innerText = fieldName[j];
             }
           }
+
           else{
-            for (let j = 0; j < iter.length; j++) {
+            for (let j = 0; j < fieldName.length; j++) {
             
               let td = document.createElement('td');
               tr.appendChild(td);
-              if (iter[j] == 'avMark') {
+
+              if (fieldName[j] == 'avMark') {
+
                   td.className = 'avMark';
             }
+
             td.innerText = getField(i, j);
             }
           }
@@ -77,20 +83,30 @@ return{
 
     },
     avMark : function() {
+
       let sum = 0;
-      let markArr = document.querySelectorAll('.avMark')
+      let markArr = document.querySelectorAll('.avMark');
+      
       for (let i = 0; i < markArr.length; i++) {
+
           let mark = +markArr[i].innerText;
           sum = sum + mark;
       }
+
       let result = sum / Student.length;
 
       return result.toFixed(2);
     }
   }
+
 }())
 
-module.appendTable();
+let h10 = document.createElement('h1');
+let h1 = body.appendChild(h10);
+h1.innerText = "Лабораторная работа №2";
+
+module.printTable();
+
 let h30 = document.createElement('h3');
 let h3 = body.appendChild(h30);
 h3.innerText = "\nAverage mark: " + module.avMark();
